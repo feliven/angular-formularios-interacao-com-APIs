27b9a5eb04d5b8a9831edbbe1c24e0b205edc2e7
FVS-MBA <feliven@gmail.com>
2025-12-08 15:53:08 -0300

    first commit

---
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-card-busca",
+  templateUrl: "./card-busca.component.html",
+  styleUrls: ["./card-busca.component.scss"],
+  standalone: false,
+})
+export class CardBuscaComponent {}
