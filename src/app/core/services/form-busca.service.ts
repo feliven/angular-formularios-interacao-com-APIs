20aefdecee3826e094ccb7eee8a7f148f4961f9d
FVS <feliven@gmail.com>
2025-12-10 12:42:06 -0300

    implementa getDescricaoPassageiros()

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -16,7 +16,85 @@ export class FormBuscaService {
       origem: new FormControl(null),
       destino: new FormControl(null),
       classe: new FormControl("Econômica"),
+      adultos: new FormControl(0),
+      criancas: new FormControl(3),
+      bebes: new FormControl(4),
     });
+  }
+
+  getDescricaoPassageiros(): string {
+    let descricao!: string;
+
+    let descricaoAdultos!: string;
+    let descricaoCriancas!: string;
+    let descricaoBebes!: string;
+
+    const numeroAdultos = this.formBusca.get("adultos")?.value;
+    const numeroCriancas = this.formBusca.get("criancas")?.value;
+    const numeroBebes = this.formBusca.get("bebes")?.value;
+
+    if (numeroAdultos && numeroAdultos > 0) {
+      if (numeroAdultos > 1) {
+        descricaoAdultos = numeroAdultos + " adultos";
+      } else {
+        descricaoAdultos = numeroAdultos + " adulto";
+      }
+    }
+
+    if (numeroCriancas && numeroCriancas > 0) {
+      if (numeroCriancas > 1) {
+        descricaoCriancas = numeroCriancas + " crianças";
+      } else {
+        descricaoCriancas = numeroCriancas + " criança";
+      }
+    }
+
+    if (numeroBebes && numeroBebes > 0) {
+      if (numeroBebes > 1) {
+        descricaoBebes = numeroBebes + " bebês";
+      } else {
+        descricaoBebes = numeroBebes + " bebê";
+      }
+    }
+
+    const colocarVirgula = ", ";
+
+    if (descricaoAdultos && descricaoCriancas && descricaoBebes) {
+      descricao = descricaoAdultos + colocarVirgula + descricaoCriancas + colocarVirgula + descricaoBebes;
+      return descricao;
+    }
+
+    if (descricaoAdultos && !descricaoCriancas && !descricaoBebes) {
+      descricao = descricaoAdultos;
+      return descricao;
+    }
+
+    if (descricaoAdultos && descricaoCriancas && !descricaoBebes) {
+      descricao = descricaoAdultos + colocarVirgula + descricaoCriancas;
+      return descricao;
+    }
+
+    if (descricaoAdultos && !descricaoCriancas && descricaoBebes) {
+      descricao = descricaoAdultos + colocarVirgula + descricaoBebes;
+      return descricao;
+    }
+
+    if (!descricaoAdultos && descricaoCriancas && !descricaoBebes) {
+      descricao = descricaoCriancas;
+      return descricao;
+    }
+
+    if (!descricaoAdultos && !descricaoCriancas && descricaoBebes) {
+      descricao = descricaoBebes;
+      return descricao;
+    }
+
+    if (!descricaoAdultos && descricaoCriancas && descricaoBebes) {
+      descricao = descricaoCriancas + colocarVirgula + descricaoBebes;
+      return descricao;
+    }
+
+    return "1 adulto";
   }
 
   obterControle(nome: string): FormControl {
