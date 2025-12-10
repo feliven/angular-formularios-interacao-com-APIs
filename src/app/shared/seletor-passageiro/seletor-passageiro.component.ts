7e48455049381a25eaeffd2be0e50b4a29a25d83
FVS <feliven@gmail.com>
2025-12-10 15:16:47 -0300

    implementa ControlValueAccessor, decrementar() e incrementar()

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,14 +1,57 @@
-import { Component, Input } from "@angular/core";
+import { Component, forwardRef, Input } from "@angular/core";
 import { BotaoControleComponent } from "../botao-controle/botao-controle.component";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
 @Component({
   selector: "app-seletor-passageiro",
   imports: [BotaoControleComponent],
   templateUrl: "./seletor-passageiro.component.html",
   styleUrl: "./seletor-passageiro.component.scss",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SeletorPassageiroComponent),
+      multi: true,
+    },
+  ],
 })
-export class SeletorPassageiroComponent {
+export class SeletorPassageiroComponent implements ControlValueAccessor {
   @Input() titulo!: string;
   @Input() subtitulo!: string;
-  @Input() quantidade!: number;
+
+  quantidade: number = 0;
+  onChange = (valor: number) => {};
+  onTouched = () => {};
+
+  writeValue(valor: number): void {
+    this.quantidade = valor;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error("Method not implemented.");
+  }
+
+  decrementar() {
+    if (this.quantidade > 0) {
+      this.quantidade -= 1;
+      this.onChange(this.quantidade);
+      this.onTouched();
+      console.log(this.quantidade);
+    }
+  }
+
+  incrementar() {
+    this.quantidade += 1;
+    this.onChange(this.quantidade);
+    this.onTouched();
+    console.log(this.quantidade);
+  }
 }
